9df607a73aa193bda7e5f6ab8dff5319ebb1da48
anderssh <anders.s.helmen@gmail.com>
2017-03-07 13:21:04 +0100

    Initial commit

---
+++ src/theProject/Main.java
@@ -0,0 +1,13 @@
+package theProject;
+
+import theProject.MySQLAccess;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		MySQLAccess dao = new MySQLAccess();
+		dao.readDataBase();
+		//PropertyHandling jee = new PropertyHandling();
+		//jee.CreateDatabaseProperies();
+		//jee.LoadDatabaseProperies();
+	}
+}
