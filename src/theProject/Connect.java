445b5e2692f6fbbd6e7e0f88209d594fc49d2a12
Harald Gard Halvorssønn Kjørholt <haraldkjorholt@Haralds-MacBook-Pro.local>
2017-03-13 10:03:29 +0100

    Harald added his files

---
+++ src/theProject/Connect.java
@@ -0,0 +1,30 @@
+package project2;
+
+import java.sql.*;
+
+public class Connect {
+	public static Connection connection;
+	public static Connection ConnectToDB() {
+		
+		//connecting to database
+		String url = "jdbc:mysql://localhost:3306/project2";
+		String username = "rall";
+		String password = "rallstad";
+
+		System.out.println("Connecting database...");
+
+		try {
+			Connection connection = DriverManager.getConnection(url, username, password);
+		    System.out.println("Database connected!");
+		    return connection;
+		} 
+		
+		catch (SQLException e) {
+		    throw new IllegalStateException("Cannot connect the database!", e);
+		}
+		
+	}
+	
+}
+
+
---
+++ src/theProject/View.java
@@ -0,0 +1,30 @@
+package project2;
+
+import java.sql.*;
+
+public class View {
+	//private Connection connection;
+
+	public static void viewTable(Connection connection) {
+
+		Statement stmt = null;
+	    String query = "select navn " +
+	                   "from " + "project2" + ".idrett";
+
+		try {
+			stmt = connection.createStatement();
+	        ResultSet rs = stmt.executeQuery(query);
+	        while (rs.next()) {
+	            String idrettsnavn = rs.getString("navn");
+	            System.out.println(idrettsnavn);
+	        }
+		} 
+		
+		catch (SQLException e) {
+		    throw new IllegalStateException("Something is fuuucked!", e);
+		}    
+
+	}
+	
+
+}
